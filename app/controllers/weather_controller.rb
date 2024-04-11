2dc35387f49a9681015666ba6b233a26fbbd8fad
Dillon Welch <daw0328@gmail.com>
2024-04-11 04:57:51 -0400

    Error

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -6,38 +6,37 @@ class WeatherController < ApplicationController
   def search
     # TODO: docs in README.md
     #
-    # TODO: validate zip / presence of values
-    # TODO: error state
-    # TODO: cleanup JS
-    # TODO: cleanup CSS
     # TODO: caching
     # TODO: integration test
     # TODO: token leak in VCR files
     # TODO: rubocop
 
-    coordinates = GeocodingService.new(
-      street: params[:street],
-      city: params[:city],
-      state: params[:state],
-      zip: params[:zip]
-    ).coordinates
+    if params[:zip].present? || (params[:city].present? && params[:state].present?)
+      coordinates = GeocodingService.new(
+        street: params[:street],
+        city: params[:city],
+        state: params[:state],
+        zip: params[:zip]
+      ).coordinates
 
-    @temps = coordinates.map do |coordinate|
-      weather_service = WeatherService.new(
-        latitude: coordinate["latitude"],
-        longitude: coordinate["longitude"],
-        units: params[:units]
-      )
+      @temps = coordinates.map do |coordinate|
+        weather_service = WeatherService.new(
+          latitude: coordinate["latitude"],
+          longitude: coordinate["longitude"],
+          units: params[:units]
+        )
 
-      {
-        "latitude" => coordinate["latitude"],
-        "longitude" => coordinate["longitude"],
-        "current" => weather_service.current_weather,
-        "extended" => weather_service.weather_forecast
-      }
+        {
+          "latitude" => coordinate["latitude"],
+          "longitude" => coordinate["longitude"],
+          "current" => weather_service.current_weather,
+          "extended" => weather_service.weather_forecast
+        }
+      end
+    else
+      flash.now[:error] = "Either the ZIP Code or both City and State must be specified."
     end
 
-    flash.now[:error] = "OH NO MELON"
     render "index"
   end
 end
