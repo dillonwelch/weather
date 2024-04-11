13a8d97ca91a3eed78edd538ece85d16b9dc79ef
Dillon Welch <daw0328@gmail.com>
2024-04-11 00:31:22 -0400

    Make the HTTP request

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -13,14 +13,15 @@ class WeatherController < ApplicationController
     )
     base_url = "geocoding.geo.census.gov"
     base_path = "/geocoder/locations/address"
-    puts URI::HTTPS.build(host: base_url, path: base_path, query: {
+    query = URI::HTTPS.build(host: base_url, path: base_path, query: {
       street: params[:street],
       city: params[:city],
       state: params[:state],
       zip: params[:zip],
       benchmark: 2020,
       format: "json"
-    }.to_query).to_s
+    }.to_query)
+    puts Net::HTTP.get(query)
     # weather = client.current_weather(units: "imperial", zip: params[:zip])["main"]
     # @current_temp = weather["temp"]
     # @low_temp = weather["temp_min"]
