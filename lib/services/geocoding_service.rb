f6c37ded7166172a5ec61542419c7d2790707b45
Dillon Welch <daw0328@gmail.com>
2024-04-11 02:53:51 -0400

    Error

--- lib/services/geocoding_service.rb
+++ lib/services/geocoding_service.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
+
+# Uses the US Census Geocoding API to turn an address into coordinates.
 class GeocodingService
   BASE_URL = "geocoding.geo.census.gov"
   BASE_PATH = "/geocoder/locations/address"
 
+  # @param street [String] Street portion of the address.
+  # @param city [String] City portion of the address.
+  # @param state [String] State portion of the address.
+  # @param zip [String] Zip portion of the address.
   def initialize(street:, city:, state:, zip:)
     @street = street
     @city = city
@@ -10,6 +16,8 @@ class GeocodingService
     @zip = zip
   end
 
+  # Queries the API to determine the coordinates.
+  # @return [Array<Hash<String, Numeric>>] Test 123
   def coordinates
     query = URI::HTTPS.build(
       host: BASE_URL,
